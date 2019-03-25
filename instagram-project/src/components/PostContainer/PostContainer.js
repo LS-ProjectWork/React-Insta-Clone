874f858b3eb5f9d40cafdb240939037f19ffefbc
dpayton23 <darien.payton71@gmail.com>
2019-03-25 18:39:56 -0400

    updated PostContainer component

--- instagram-project/src/components/PostContainer/PostContainer.js
+++ instagram-project/src/components/PostContainer/PostContainer.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const PostContainer = props => {
-    return <div>{props.data}</div>
+    console.log(props.data)
+    return (
+        <div>
+            {props.data.map(post => {
+                return(
+                    <div className='post-header'>
+                        <img src={post.thumbnailUrl}/>
+                        <h2>{post.username}</h2>
+                        <img src={post.imageUrl}/>
+                        <p>{post.likes}</p>
+                        <p>{post.timestamp}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default PostContainer
