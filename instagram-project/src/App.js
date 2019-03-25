52f8c5b7dbe7ee89cee2b4c7a9bbecbd129f6ea7
dpayton23 <darien.payton71@gmail.com>
2019-03-25 17:22:27 -0400

    set up state object and passed data prop

--- instagram-project/src/App.js
+++ instagram-project/src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import dummyData from './dummy-Data'
 
 class App extends Component {
+  state = {data: dummyData};
+
   render() {
     return (
       <div className="App">
+        <PostContainer data={this.state.data} />
       </div>
     );
   }
