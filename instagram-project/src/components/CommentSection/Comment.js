61950e7657d29326a91d385190d7b1ab77908ad3
dpayton23 <darien.payton71@gmail.com>
2019-03-26 08:59:20 -0400

    updated component

--- instagram-project/src/components/CommentSection/Comment.js
+++ instagram-project/src/components/CommentSection/Comment.js
@@ -4,17 +4,13 @@ const Comment = props =>  {
     return (
         <div>
             return (
-                {props.data.map(comment => {
-                    return (
-                        <div key={comment.id}>
-                            <span>{comment.username}</span>
-                            <span>{comment.text}</span>
-                        </div>
-                    )
-                })}
+                <div>
+                    <span>{props.comment.username}</span>
+                    <span>{props.comment.text}</span>
+                </div>
             )
         </div>
     )
 }
 
-export default Comment
+export default Comment;
