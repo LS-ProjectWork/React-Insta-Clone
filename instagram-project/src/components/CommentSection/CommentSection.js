af2d5c0c6412919721d12e94a868afc683cb1cdb
dpayton23 <darien.payton71@gmail.com>
2019-03-26 08:58:47 -0400

    updated component

--- instagram-project/src/components/CommentSection/CommentSection.js
+++ instagram-project/src/components/CommentSection/CommentSection.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Comment from './Comment'
 
 class CommentSection extends React.Component {
-    state = {comments: props.comments}
+    constructor(props) {
+        super(props)
+        this.state = {comments: props.comments}
+    }
 
     render() {
         return (
             <div>
-                {this.state.map(comment => {
-                    return <Comment comment={this.state.comments} />
+                {this.state.comments && this.state.comments.map(comment => {
+                    return <Comment key={comment.id} comment={comment} />
                 })}
             </div>
         )
