fd3434c9df9b0403ad2a4e1457e495452db0b58e
dpayton23 <darien.payton71@gmail.com>
2019-03-27 19:27:40 -0400

    passed HOC

--- instagram-project/src/App.js
+++ instagram-project/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostContainer/PostsPage';
+import LoginPage from './components/login/login'
 import withAuthenticate from './components/authentication/withAuthenticate';
 
 
-const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage)(LoginPage);
 
 class App extends Component {
   render() {
