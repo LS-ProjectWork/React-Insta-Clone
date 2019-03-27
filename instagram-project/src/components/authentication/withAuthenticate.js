83a6d95e86307a805275a1104d92d3f336e10a9d
dpayton23 <darien.payton71@gmail.com>
2019-03-27 19:30:29 -0400

    added methods for logging in and logging out & added persisted data in localStorage

--- instagram-project/src/components/authentication/withAuthenticate.js
+++ instagram-project/src/components/authentication/withAuthenticate.js
@@ -1,10 +1,45 @@
 import React from 'react';
 
-const withAuthenticate = Component => class ClassComponent extends React.Component {
-    render() {
-        return (
-            <Component />
+const withAuthenticate = ProtectedComponent => LoginComponent => class extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            'loggedIn': false, 
+            'username': null
+        }
+    }
+
+    login = event => {
+        event.preventDefault()
+        const username = event.target.username.value
+        const password = event.target.password.value
+        
+        if (username !== "" && password !== "") {
+            localStorage.setItem('login', JSON.stringify({'loggedIn': true, 'username': username}))
+            
+            this.setState({'loggedIn': true, 'username': username})
+        }
+    }
+    
+    componentDidMount() {
+        this.setState(
+         JSON.parse(localStorage.getItem('login'))
         )
+    }
+
+    logout = () => {
+        localStorage.setItem('login', JSON.stringify({'loggedIn': false, 'username': ''}))
+
+        this.setState({'loggedIn': false, 'username': ''})
+    }
+
+    render() {
+        if(this.state.loggedIn) {
+            return (
+                <ProtectedComponent logout={this.logout} />
+            )
+        }
+        return <LoginComponent submit={this.login} login={this.login} />
     }
 }
 
